863dbb5def625e30909ddf7756b61b559b00e774
Salman1717 <salmanmhaskar17@gmail.com>
2025-12-25 23:23:53 +0530

    Initial Commit

---
+++ VinciBit/VinciBitApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinciBitApp.swift
+//  VinciBit
+//
+//  Created by Salman Mhaskar on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct VinciBitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
